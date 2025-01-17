bd2a5f39b4aa6d5e9309f2d9fb782fd9828c4d9e
Your Name <you@example.com>
2025-01-16 23:54:58 -0500

    "Tailwind Integration"

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{html,js}',
+    './subscriptions/templates/**/*.{html,js}',
+    // etc.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
